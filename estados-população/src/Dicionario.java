29062c2bf59636a47c9dc00b98cd29091a6f20be
Gabriel Silva <gabelimaa2@gmail.com>
2025-01-13 12:16:28 +0000

    2 commit

--- estados-população/src/Dicionario.java
+++ estados-população/src/Dicionario.java
@@ -1,5 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dicionario {
-    public static void main(String[] args)  {
+    public static void main(String[] args){
+
+        System.out.println("Os estados brasileiros e suas populações:");
        
+        Map<String, Double> estadosBrasileiros = new HashMap<String, Double>(){{
+
+        put("PE", (double) 9616621);
+        put("AL", (double) 3351543);
+        put("CE", (double) 9187103);
+        put("RN", (double) 3534265);
+
+        }};
+        System.out.println(estadosBrasileiros);
+
     }
 }
